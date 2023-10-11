45c884afe21980b2e91b657a0f43cc9420635d9e
Gatomek <tomasz.galinski@gmail.com>
2023-10-11 08:37:48 +0200

    Liquibase added

--- src/main/java/pl/gatomek/webdicted/entity/GermanDictEntry.java
+++ src/main/java/pl/gatomek/webdicted/entity/GermanDictEntry.java
@@ -1,12 +1,22 @@
 package pl.gatomek.webdicted.entity;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.Index;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
 
-@Table( name="GERMAN", indexes = {
-        @Index( name = "german_query_index", columnList = "REQUEST", unique = true)
+@Table( schema = "web_dicted_schema", name="GERMAN", indexes = {
+        @Index( name = "german_query_index", columnList = "REQUEST", unique = false)
 })
 @Entity
 public class GermanDictEntry extends DictEntry{
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "german_dict_sequence")
+    private Long id;
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
 }
